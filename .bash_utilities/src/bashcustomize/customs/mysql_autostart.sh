8d666469bca07713151bbb3dc5852323d23f545f
Daniel F. Rivera C <daniel.rivera@pragma.com.co>
2021-11-04 20:08:59 -0500

    Fix mysql autostart validation

--- .bash_utilities/src/bashcustomize/customs/mysql_autostart.sh
+++ .bash_utilities/src/bashcustomize/customs/mysql_autostart.sh
@@ -2,7 +2,7 @@
 
 source ~/.bash_utilities/lib/messages.sh
 
-if [ -z "$(ls -a /var/run/mysqld | grep "^mysqld.sock$")" ]; then
+if [ -z "$(ls -a /var/run | grep ^mysqld$)" ] || [ -z "$(ls -a /var/run/mysqld | grep ^mysqld.sock$)" ]; then
   printInfoMessage "MySQL service is stopped. Let's start it."
   sudo service mysql start && printInfoMessage "MySQL service started"
   sleep 2
