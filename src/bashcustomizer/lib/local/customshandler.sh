9a9aff56eb50b80199ff1629c4c0944617ae265a
Daniel F. Rivera C <daniel.rivera@pragma.com.co>
2022-01-25 13:33:41 -0500

    Define customshandler loca library for bashcustomizer

--- src/bashcustomizer/lib/local/customshandler.sh
+++ src/bashcustomizer/lib/local/customshandler.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
+
+# Check if a customization can be included
+# $1 customization name
+# $2 customization value
 function checkCustomization() {
-    # $1 customization variable name without prefix
-    var="custom_bash_$1"
-    if [ -n "${!var}" ] && [ "${!var}" == "on" ] && [ -f ~/.bash_utilities/src/bashcustomize/customs/"$1".sh ]; then
+    if [ "$2" = "on" ] && [ -f ~/.bash_utilities/src/bashcustomizer/src/"$1".sh ]; then
         echo 1
     else
         echo 0
     fi
 }
 
+# Includes a customization on execution
+# $1 customization name
 function includeCustomization() {
-    # $1 customization name
     if [ ! -z "$1" ]; then
-        . ~/.bash_utilities/src/bashcustomize/customs/"$1".sh
+        . ~/.bash_utilities/src/bashcustomizer/src/"$1".sh
     fi
 }
