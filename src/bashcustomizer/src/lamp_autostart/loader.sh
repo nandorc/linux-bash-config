0457d6e27e69d95cf4e550416fe042a98b67dc89
Daniel F. Rivera C <daniel.rivera@pragma.com.co>
2022-01-26 16:31:20 -0500

    Update lamp_autostart customization loading

--- src/bashcustomizer/src/lamp_autostart/loader.sh
+++ src/bashcustomizer/src/lamp_autostart/loader.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
+# Dependencies
 source ~/.bash_utilities/src/bashcustomizer/lib/local/customshandler.sh
-source ~/.bash_utilities/lib/ext/dsoft/messages.sh
 
-printMessage "Checking LAMP services..."
+# Included customizations
 includeCustomization apache_autostart
 includeCustomization mysql_autostart
-printMessage "LAMP services running."
