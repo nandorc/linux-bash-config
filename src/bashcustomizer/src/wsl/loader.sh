147da152001aacf3b853e884432436951fbdef35
Daniel F. Rivera C <daniel.rivera@pragma.com.co>
2022-01-26 13:57:15 -0500

    Update wsl customizations loading

---
+++ src/bashcustomizer/src/wsl/loader.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Dependencies
+source ~/.bash_utilities/src/bashcustomizer/lib/local/executor.sh
+
+# Aliases
+wrapFileInclusion "Including WSL aliases..." "~/.bash_utilities/src/bashcustomizer/src/wsl/aliases.sh"
