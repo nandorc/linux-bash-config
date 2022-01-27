9f585ac3e30d908ecf479bcb0955e48f7a3b129c
Daniel F. Rivera C <daniel.rivera@pragma.com.co>
2022-01-26 19:09:47 -0500

    Update docker customization loading

--- src/bashcustomizer/src/docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source ~/.bash_utilities/lib/ext/dsoft/messages.sh
-
-# Docker Service
-alias docker_start="printInfoMessage 'Starting docker service.' before && sudo service docker start && printInfoMessage 'Docker service started.' after"
-alias docker_stop="printInfoMessage 'Stopping docker service.' before && sudo service docker stop && printInfoMessage 'Docker service stopped.' after"
-alias docker_restart="printInfoMessage 'Restarting docker service.' before && sudo service docker restart && printInfoMessage 'Docker service restarted.' after"
