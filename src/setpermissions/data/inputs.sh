d738439311bc63f595dc02cb9f71a62bc5d301e2
Daniel F. Rivera C <daniel.rivera@pragma.com.co>
2022-01-27 10:33:16 -0500

    Define help command for setpermissions tool

---
+++ src/setpermissions/data/inputs.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+showhelp=0
+while [ -n "$1" ]; do
+    case "$1" in
+    -h)
+        showhelp=1
+        ;;
+    -owner)
+        ownername="$2"
+        shift
+        ;;
+    -group)
+        groupname="$2"
+        shift
+        ;;
+    -file)
+        filperm="$2"
+        shift
+        ;;
+    -folder)
+        dirperm="$2"
+        shift
+        ;;
+    -exe)
+        exeperm="$2"
+        shift
+        ;;
+    --)
+        shift
+        break
+        ;;
+    *)
+        path="$1"
+        break
+        ;;
+    esac
+    shift
+done
