9a92520a97348cd942024f4dcd3a35fd4c617cd7
Daniel F. Rivera C <daniel.rivera@pragma.com.co>
2022-01-26 19:03:51 -0500

    Update magento_autostart customization loading

--- src/bashcustomizer/src/magento_autostart/loader.sh
+++ src/bashcustomizer/src/magento_autostart/loader.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
+# Dependencies
 source ~/.bash_utilities/src/bashcustomizer/lib/local/customshandler.sh
-source ~/.bash_utilities/lib/ext/dsoft/messages.sh
 
-printMessage "Checking Magento services..."
+# Included customizations
 includeCustomization lamp_autostart
 includeCustomization elasticsearch_autostart
-printMessage "Magento services running."
