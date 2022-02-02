81b642cb41a6d33c9c2aff84abecf61cc7f5b132
Daniel F. Rivera C <dsoftcolombia@gmail.com>
2022-02-02 15:28:31 -0500

    Update commands dispatching

--- src/boot/README.md
+++ src/boot/README.md
@@ -5,3 +5,7 @@ This command allow users to manage on boot services checking and starting.
 `basher boot --help`
 
 > Prints this information about command options.
+
+`basher boot enable [feature]`
+
+> Enable boot feature check. [feature] is the name of the intended feature to be enabled. If no [feature] given, enables all features.
--- src/boot/main.sh
+++ src/boot/main.sh
@@ -2,9 +2,12 @@
 
 # Dependencies
 source ~/.basher/lib/messages.sh
+source ~/.basher/lib/flagger.sh
 
 if [ "$1" = "--help" ]; then
     basher mdv ~/.basher/src/boot/README.md
+elif [ "$1" == "enable" ]; then
+    ~/.basher/src/boot/cmd/enablefeature.sh $(pruneFlag enable $*)
 else
     printErrorMessage "No valid options received." before
     printWarningMessage "Type 'basher boot --help' to see available options." after
