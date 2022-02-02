fb6800c67f4d1ce7878804a0121771292a0ca8c2
Daniel F. Rivera C <dsoftcolombia@gmail.com>
2022-02-02 18:30:42 -0500

    Update basher boot:reset command

--- src/boot/cmd/reset.sh
+++ src/boot/cmd/reset.sh
@@ -2,17 +2,10 @@
 
 # Dependencies
 source ~/.basher/lib/inihandler.sh
-source ~/.basher/lib/flagger.sh
 source ~/.basher/lib/wrapper.sh
 
-# Set variables
-useColor=1
-[ $(hasFlag --no-color $*) -eq 1 ] && useColor=0
-spacing=$(getFlagValue --spacing $*)
-[ -z "$spacing" ] && spacing=both
-
 # Begin message
-printColoredMessage "Resetting features.ini file..." $(transformBeforeSpacing $spacing) $useColor
+printColoredMessage "Resetting features.ini file..." --wrap-position begin $*
 
 # Delete current features.ini file if exists
 if [ -f ~/.basher/src/boot/etc/features.ini ]; then
@@ -29,7 +22,4 @@ setINIVar ~/.basher/src/boot/etc/features.ini git on
 printMessage "* Default values stored in features.ini file."
 
 # End message
-printColoredMessage "done" $(transformAfterSpacing $spacing) $useColor
-
-# Unset variables
-unset useColor spacing
+printColoredMessage "done" --wrap-position end $*
