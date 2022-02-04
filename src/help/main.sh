78bb7704319e91df98c0f342cf525e77b956dbef
Daniel F. Rivera C <dsoftcolombia@gmail.com>
2022-02-04 09:12:56 -0500

    Define basher help command behaviour

--- src/help/main.sh
+++ src/help/main.sh
@@ -9,5 +9,9 @@ elif [ "$1" = "list" ]; then
     ~/.basher/src/help/cmd/list.sh
 elif [ ! -d ~/.basher/src/"$1" ]; then
     printErrorMessage "'$1' is not a valid command or component" before
-    printMessage "To get a list of available commands and components type: basher help:list" after
+    printMessage "To get a list of available commands and components type 'basher help:list'" after
+elif [ ! -f ~/.basher/src/"$1"/README.md ]; then
+    printErrorMessage "'$1' doesn't hava documentation" both
+else
+    basher mdv ~/.basher/src/"$1"/README.md
 fi
