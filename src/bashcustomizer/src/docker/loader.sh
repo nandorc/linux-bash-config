ecc519acf7f7c9eb02e07d1628c2278b67fca217
Daniel F. Rivera C <dsoftcolombia@gmail.com>
2022-02-04 15:50:39 -0500

    Update basher docker command

--- src/bashcustomizer/src/docker/loader.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Dependencies
-source ~/.basher/src/bashcustomizer/lib/local/executor.sh
-
-# Load aliases
-wrapFileInclusion "Including docker aliases..." "~/.basher/src/bashcustomizer/src/docker/aliases.sh"
