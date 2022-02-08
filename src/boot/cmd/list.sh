ec72a31460d31a1415628dc0905f158f152ec271
Daniel F. Rivera C <dsoftcolombia@gmail.com>
2022-02-07 19:15:01 -0500

    Define basher boot:list command

---
+++ src/boot/cmd/list.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Dependencies
+source ~/.basher/lib/wrapper.sh
+
+options=$(getRebuildedOptions $*)
+current_values=""
+printColoredMessage "Getting currently defined values at features.ini file..." --wrap-position begin $options
+[ -f ~/.basher/src/boot/etc/features.ini ] && current_values=$(cat ~/.basher/src/boot/etc/features.ini)
+if [ -z "$current_values" ]; then
+    printColoredMessage "No values defined" --wrap-position end --type warning $(pruneFlag --no-color $options)
+else
+    printColoredMessage "$current_values" --wrap-position end --no-color $(pruneFlag --no-color $options)
+fi
+unset options current_values
