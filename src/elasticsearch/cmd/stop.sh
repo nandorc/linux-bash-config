ead8c126e59daa12c60f614a1051c7601836cbb0
Daniel F. Rivera C <dsoftcolombia@gmail.com>
2022-02-08 13:37:07 -0500

    Fix basher elasticsearch:stop command on java services validation

--- src/elasticsearch/cmd/stop.sh
+++ src/elasticsearch/cmd/stop.sh
@@ -11,21 +11,27 @@ if [ $(basher elasticsearch:status --output bool) -eq 0 ]; then
 elif [ ! -f ~/.basher/src/elasticsearch/var/pid ]; then
     warning=" * stopping elasticsearch service fails because it wasn't started by basher"
 else
-    java_services=$(pgrep java)
-    service_pid=$(echo $java_services | grep "^$(cat ~/.basher/src/elasticsearch/var/pid)$")
-    if [ -z "$service_pid" ] && [ ! -z "$java_services" ]; then
-        warning=" * elasticsearch service pid doesn't match so basher isn't able to stop it"
-    elif [ -z "$java_services" ]; then
+    javaServices=$(pgrep java) && javaServicesArray=(${javaServices// / })
+    if [ -z "$javaServices" ]; then
         warning=" * no java services currently running so basher isn't able to stop elasticsearch service"
     else
-        pkill -F ~/.basher/src/elasticsearch/var/pid
-        while [ $(basher elasticsearch:status --output bool) -eq 1 ]; do
-            sleep 2
+        servicePid=$(cat ~/.basher/src/elasticsearch/var/pid) && serviceFound=0
+        for i in "${javaServicesArray[@]}"; do
+            [ "$i" = "$servicePid" ] && serviceFound=1 && break
         done
-        rm -f ~/.basher/src/elasticsearch/var/pid
-        printColoredMessage " * elasticsearch service stopped" --wrap-position end --no-color $options
+        if [ $serviceFound -eq 0 ]; then
+            warning=" * elasticsearch service pid doesn't match so basher isn't able to stop it"
+        else
+            pkill -F ~/.basher/src/elasticsearch/var/pid
+            while [ $(basher elasticsearch:status --output bool) -eq 1 ]; do
+                sleep 2
+            done
+            rm -f ~/.basher/src/elasticsearch/var/pid
+            printColoredMessage " * elasticsearch service stopped" --wrap-position end --no-color $options
+        fi
+        unset servicePid serviceFound
     fi
-    unset service_pid java_services
+    unset javaServices javaServicesArray
 fi
 [ -n "$warning" ] && echo "$warning" >>~/.basher/src/elasticsearch/var/warning && printColoredMessage "$warning" --type warning --wrap-position end $(pruneFlag --no-color $options)
 unset options warning
