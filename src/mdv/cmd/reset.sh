4b54ba448767be74ec762e0fc59e301e8b16839d
Daniel F. Rivera C <dsoftcolombia@gmail.com>
2022-02-09 08:08:09 -0500

    Update config.ini file location on basher mdv

--- src/mdv/cmd/reset.sh
+++ src/mdv/cmd/reset.sh
@@ -5,21 +5,18 @@ source ~/.basher/lib/messages.sh
 
 # Create config.ini file if is first use or clean it if already exists
 printInfoMessage "Preparing config.ini file..." before
-if [ ! -d ~/.basher/src/mdv/etc ]; then
-    mkdir -p ~/.basher/src/mdv/etc
-fi
-if [ ! -f ~/.basher/src/mdv/etc/config.ini ]; then
-    touch ~/.basher/src/mdv/etc/config.ini
+if [ ! -f ~/.basher/src/mdv/var/config.ini ]; then
+    touch ~/.basher/src/mdv/var/config.ini
     printMessage "* config.ini file created."
 else
-    echo "" >~/.basher/src/mdv/etc/config.ini
+    echo "" >~/.basher/src/mdv/var/config.ini
     printMessage "* config.ini file cleaned."
 fi
 
 # Write header information at config.ini file
-echo "# Use pandoc as MarkDown Viewer (recommended)." >~/.basher/src/mdv/etc/config.ini
-echo "#   In case you decide to use pandoc and is not installed, it will be installed on your machine, so you will need sudo access." >>~/.basher/src/mdv/etc/config.ini
-echo "#   If you decide to not use pandoc or it couln't be installed, less will be used as MarkDown viewer." >>~/.basher/src/mdv/etc/config.ini
+echo "# Use pandoc as MarkDown Viewer (recommended)." >~/.basher/src/mdv/var/config.ini
+echo "#   In case you decide to use pandoc and is not installed, it will be installed on your machine, so you will need sudo access." >>~/.basher/src/mdv/var/config.ini
+echo "#   If you decide to not use pandoc or it couln't be installed, less will be used as MarkDown viewer." >>~/.basher/src/mdv/var/config.ini
 printMessage "* config.ini file headers were written."
 
 # Ask user if want to use pandoc
@@ -34,6 +31,6 @@ while [ $use_pandoc -eq -1 ]; do
         printWarningMessage "Wrong answer! Valid options are y or n. Try again."
     fi
 done
-echo "use_pandoc=$use_pandoc" >>~/.basher/src/mdv/etc/config.ini
+echo "use_pandoc=$use_pandoc" >>~/.basher/src/mdv/var/config.ini
 unset user_answer use_pandoc
 printInfoMessage "config.ini file ready" after
