6d4dfc57410faec1013e8634cd0531ba583b4bb2
Daniel F. Rivera C <daniel.rivera@pragma.com.co>
2022-05-26 11:56:27 -0500

    Delete deprecated files

--- etc/colors.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-color_none="\e[0m"
-color_red="\e[31m"
-color_green="\e[32m"
-color_yellow="\e[33m"
-color_blue="\e[34m"
--- src/config/README.md
+++
@@ -1,3 +0,0 @@
-# basher config
-
-Controls loading of variables, aliases, styles and settings on boot process.
