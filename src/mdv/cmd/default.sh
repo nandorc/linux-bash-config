aefbbc74cb7e7b1f8fa39c3c8043982b19373e23
Daniel F. Rivera C <daniel.rivera@pragma.com.co>
2022-05-26 20:04:33 -0500

    Refactor messages for basher mdv command

--- src/mdv/cmd/default.sh
+++ src/mdv/cmd/default.sh
@@ -1,15 +1,70 @@
 #!/bin/bash
 
 # Load dependencies
-source ~/.basher/etc/colors.sh
+source ~/.basher/lib/colorhandler.sh
 source ~/.basher/lib/inihandler.sh
+source ~/.basher/lib/flagger.sh
 
 # Define parameters
-file_path=$1
+[ $# -eq 0 ] && echo -e "\n$(color red)Execution Exception:$(color none) Path to file not defined\nType $(color yellow)basher help mdv$(color none) to know how to use the command\n" && exit 1
+parameters=$*
+must_reset=$(hasFlag --reset $parameters) && parameters=$(pruneFlag --reset $parameters)
+parameters=(${parameters// / })
+[ ${#parameters[@]} -ne 1 ] && echo -e "\n$(color red)Execution Exception:$(color none) Invalid parameters received\nType $(color yellow)basher help mdv$(color none) to know how to use the command\n" && exit 1
+file_path=${parameters[0]}
+[ ! -f "$file_path" ] && echo -e "\n$(color red)Execution Exception:$(color none) File not found at $(color yellow)$file_path$(color none)\n" && exit 1
 
-# Validations
-[ -z "$file_path" ] && echo -e "\n"$color_red"Execution Exception: "$color_none"Path to file not defined\nType "$color_yellow"basher help mdv"$color_none" to know how to use the command\n" && exit 1
-[ ! -f "$file_path" ] && echo -e "\n"$color_red"Execution Exception: "$color_none"File not found at "$color_yellow"$file_path\n" && exit 1
+# Validate if ~/.basher/src/mdv/var/config.ini needs to be resetted
+[ $must_reset -eq 0 ] && [ ! -f ~/.basher/src/mdv/var/config.ini ] && must_reset=1
+is_invalid_value=0
+use_pandoc=$(getINIVar ~/.basher/src/mdv/var/config.ini use_pandoc)
+[ "$use_pandoc" = "undefined" ] || [ -z "$use_pandoc" ] && is_invalid_value=1
+[ "$use_pandoc" != "0" ] && [ "$use_pandoc" != "1" ] && is_invalid_value=1
+[ $must_reset -eq 0 ] && [ $is_invalid_value -eq 1 ] && must_reset=1
+
+# Reset config.ini file if necessary
+if [ $must_reset -eq 1 ]; then
+    # Inform if value is invalid
+    [ $is_invalid_value -eq 1 ] && echo -e "\n$(color yellow)WARNING!$(color none) config.ini file must be resetted due to not valid value setted\n"
+
+    # Create config.ini file if is first use or clean it if already exists
+    is_first_set=0
+    [ ! -f ~/.basher/src/mdv/var/config.ini ] && is_first_set=1
+    echo -e "\n$(color green)START: $(color none)\c"
+    if [ $is_first_set -eq 1 ]; then
+        touch ~/.basher/src/mdv/var/config.ini
+        echo -e "Setting mdv configuration"
+    else
+        echo "" >~/.basher/src/mdv/var/config.ini
+        echo -e "Resetting mdv configuration"
+    fi
+
+    # Write header information at config.ini file
+    echo "# Use pandoc as MarkDown Viewer (recommended)." >~/.basher/src/mdv/var/config.ini
+    echo "#   In case you decide to use pandoc and is not installed, it will be installed on your machine, so you will need sudo access." >>~/.basher/src/mdv/var/config.ini
+    echo "#   If you decide to not use pandoc or it couln't be installed, less will be used as MarkDown viewer." >>~/.basher/src/mdv/var/config.ini
+    echo -e "$(color blue)INFO:$(color none) config.ini file headers were written\n"
+
+    # Ask user if want to use pandoc
+    use_pandoc=-1
+    while [ $use_pandoc -eq -1 ]; do
+        read -p "Would you like to use pandoc to view Markdown files? (Recomended) [y/n]: " user_answer
+        if [ "$user_answer" = "y" ] || [ "$user_answer" = "Y" ]; then
+            use_pandoc=1
+        elif [ "$user_answer" = "n" ] || [ "$user_answer" = "N" ]; then
+            use_pandoc=0
+        else
+            echo -e "$(color yellow)Wrong answer!$(color none) Valid options are y or n\nTry again"
+        fi
+    done
+    echo "use_pandoc=$use_pandoc" >>~/.basher/src/mdv/var/config.ini
+    echo -e "\n$(color green)FINISH:$(color none) mdv configuration \c"
+    if [ $is_first_set -eq 1 ]; then
+        echo -e "setted\n"
+    else
+        echo -e "resetted\n"
+    fi
+fi
 
 # Check if pandoc must be used
 # returns:
@@ -18,12 +73,10 @@ file_path=$1
 #   -1 :: pandoc can be used but is not installed
 function canUsePandoc() {
     use_pandoc=$(getINIVar ~/.basher/src/mdv/var/config.ini use_pandoc)
-    if [ -z "$use_pandoc" ] || [ "$use_pandoc" = "undefined" ] || [ $use_pandoc -eq 0 ]; then
-        echo 0
-    elif [ -z "$(whereis pandoc | sed -e "s/pandoc://" -e "s/ //g")" ]; then
+    if [ $use_pandoc -eq 1 ] && [ -z "$(whereis pandoc | sed -e "s/pandoc://" -e "s/ //g")" ]; then
         echo -1
     else
-        echo 1
+        echo $use_pandoc
     fi
 }
 use_pandoc=$(canUsePandoc)
@@ -35,7 +88,13 @@ use_pandoc=$(canUsePandoc)
 [ $use_pandoc -eq 1 ] && pandoc -f markdown -t plain "$file_path" | less -c && exit
 
 # Try to install pandoc
-echo -e "\n"$color_yellow"WARNING!"$color_none" Pandoc is not installed!\nYou must type sudo password to install pandoc"
-sudo echo -e "Trying to install "$color_yellow"pandoc"$color_none"..." && sudo apt update && sudo apt install pandoc
-[ $(canUsePandoc) -eq -1 ] && echo -e "\n"$color_red"ERROR:"$color_none" Something failed while installing "$color_yellow"pandoc\nless"$color_none" will be used to show files\n" && setINIVar ~/.basher/src/mdv/var/config.ini use_pandoc 0
+[ $must_reset -eq 0 ] && echo -e ""
+echo -e "$(color yellow)WARNING!$(color none) Pandoc is not installed!\nYou must type sudo password to install pandoc"
+sudo echo -e "$(color green)START:$(color none) Trying to install $(color yellow)pandoc$(color none)" && sudo apt update && sudo apt install pandoc
+if [ $(canUsePandoc) -eq -1 ]; then
+    echo -e "$(color red)FAIL:$(color none) Something went wrong while installing $(color yellow)pandoc$(color none) so $(color yellow)less$(color none) will be used to show files\n"
+    setINIVar ~/.basher/src/mdv/var/config.ini use_pandoc 0
+else
+    echo -e "$(color green)FINISH: $(color yellow)pandoc$(color none) was installed\n"
+fi
 basher mdv "$file_path"
