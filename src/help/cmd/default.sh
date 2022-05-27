12684bf048e334d86a211d1c9150421a9bb6fc3c
Daniel F. Rivera C <daniel.rivera@pragma.com.co>
2022-05-26 20:23:20 -0500

    Refactor messages for basher help command

--- src/help/cmd/default.sh
+++ src/help/cmd/default.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Load dependencies
-source ~/.basher/etc/colors.sh
+source ~/.basher/lib/colorhandler.sh
 
 # Get command name
 command_name=$1
@@ -10,10 +10,10 @@ command_name=$1
 [ -z "$command_name" ] && basher mdv ~/.basher/src/help/README.md && exit
 
 # Check if is a valid component or command
-[ ! -d ~/.basher/src/"$command_name" ] && echo -e "\n"$color_red"Execution Exception: "$color_yellow"$command_name"$color_none" is not a valid command or component\nType "$color_yellow"basher help:list"$color_none" to get a list of available commands and components\n" && exit 1
+[ ! -d ~/.basher/src/"$command_name" ] && echo -e "\n$(color red)Execution Exception: $(color yellow)$command_name$(color none) is not a valid command or component\nType $(color yellow)basher help:list$(color none) to get a list of available commands and components\n" && exit 1
 
 # Check if README file exists
-[ ! -f ~/.basher/src/"$command_name"/README.md ] && echo -e "\n"$color_red"Execution Exception: "$color_yellow"$command_name"$color_none" doesn't have documentation\n" && exit 1
+[ ! -f ~/.basher/src/"$command_name"/README.md ] && echo -e "\n$(color red)Execution Exception: $(color yellow)$command_name$(color none) doesn't have documentation\n" && exit 1
 
 # Show help for specific command
 basher mdv ~/.basher/src/"$command_name"/README.md
