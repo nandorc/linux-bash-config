a3a4fc24c5a943cb83820984936745494a6fa328
Daniel F. Rivera C <daniel.rivera@pragma.com.co>
2022-05-30 14:01:47 -0500

    Refactor messagehandler library

--- lib/messagehandler.sh
+++ lib/messagehandler.sh
@@ -5,69 +5,84 @@ source ~/.basher/lib/colorhandler.sh
 
 # Wraps messages for services actions
 # $1 message
-# $2 no-spaces before
-# $3 no-spaces after
-# $4 must-skip
+# $2 has_space_before
+# $3 has_space_after
+# $4 is_silent
 # $5 is_exception
 function wrap() {
-    declare message=$1 && declare no_spaces_before=$2 && declare no_spaces_after=$3 && declare must_skip=$4 && declare is_exception=$5
-    [ -n "${no_spaces_before}" ] && [ "${no_spaces_before}" != "0" ] && no_spaces_before=1
-    [ -z "${no_spaces_before}" ] && no_spaces_before=0
-    [ -n "${no_spaces_after}" ] && [ "${no_spaces_after}" != "0" ] && no_spaces_after=1
-    [ -z "${no_spaces_after}" ] && no_spaces_after=0
-    [ -n "${must_skip}" ] && [ "${must_skip}" != "0" ] && must_skip=1
-    [ -z "${must_skip}" ] && must_skip=0
+    declare message has_space_before has_space_after is_silent is_exception
+    message=$1 && has_space_before=$2 && has_space_after=$3 && is_silent=$4 && is_exception=$5
+    [ -n "${has_space_before}" ] && [ "${has_space_before}" != "0" ] && has_space_before=1
+    [ -z "${has_space_before}" ] && has_space_before=0
+    [ -n "${has_space_after}" ] && [ "${has_space_after}" != "0" ] && has_space_after=1
+    [ -z "${has_space_after}" ] && has_space_after=0
+    [ -n "${is_silent}" ] && [ "${is_silent}" != "0" ] && is_silent=1
+    [ -z "${is_silent}" ] && is_silent=0
     [ -n "${is_exception}" ] && [ "${is_exception}" != "0" ] && is_exception=1
     [ -z "${is_exception}" ] && is_exception=0
-    [ ${no_spaces_before} -eq 0 ] && echo ""
-    [ ${must_skip} -eq 0 ] && echo -e "${message}"
-    [ ${no_spaces_after} -eq 0 ] && echo ""
+    [ ${has_space_before} -eq 1 ] && echo ""
+    [ ${is_silent} -eq 0 ] && echo -e "${message}"
+    [ ${has_space_after} -eq 1 ] && echo ""
     [ ${is_exception} -eq 1 ] && exit 1
-    unset
+    return 0
 }
 
-# Returns a message based on response code
-# $1 response code
-function getResponseMessage() {
-    declare res_cod=$1
+# Returns a string based on response code
+# $1 res_cod
+function getResponseString() {
+    declare res_cod
+    res_cod=$1
     [ $res_cod -eq 0 ] && echo "$(color green)DONE$(color)"
     [ $res_cod -ne 0 ] && echo "$(color red)FAIL$(color)"
-    unset
+    return 0
 }
 
 # Displays a message based on response code
-# $1 response code
-function processResponse() {
-    declare res_cod=$1
-    echo -e "$(getResponseMessage ${res_cod})"
-    unset
+# $1 res_cod
+function showResponseString() {
+    declare res_cod
+    res_cod=$1
+    echo -e "$(getResponseString ${res_cod})"
+    return 0
+}
+
+# Get a generic message to point user to helper command
+# $1 command_name
+function getCommandHelpInfoMessage() {
+    declare command_name
+    command_name=$1
+    echo "$(color light-blue)INF~$(color) Type $(color yellow)basher help ${command_name}$(color) to know how to use the command"
+    return 0
 }
 
 # Displays an Exception
 # $1 message
-# $2 no-spaces-before
-# $3 no-spaces-after
+# $2 has_space_before
+# $3 has_space_after
 function genericException() {
-    declare message=$1 && declare no_spaces_before=$2 && declare no_spaces_after=$3
-    wrap "$(color red)ERR~$(color) ${message}" ${no_spaces_before} ${no_spaces_after} 0 1
-    unset
+    declare message has_space_before has_space_after
+    message=$1 && has_space_before=$2 && has_space_after=$3
+    wrap "$(color red)ERR~$(color) ${message}" "${has_space_before}" "${has_space_after}" 0 1
+    return 0
 }
 
 # Displays no valid options message
-# $1 command name
-# $2 no-spaces-before
-# $3 no-spaces-after
+# $1 command_name
+# $2 has_space_before
+# $3 has_space_after
 function noValidOptionsException() {
-    declare command_name=$1 && declare no_spaces_before=$2 && declare no_spaces_after=$3
-    genericException "No valid parameters sent to command\n$(color light-blue)INF~$(color) Type $(color yellow)basher help ${command_name}$(color) to know how to use the command" ${no_spaces_before} ${no_spaces_after}
-    unset
+    declare command_name has_space_before has_space_after
+    command_name=$1 && has_space_before=$2 && has_space_after=$3
+    genericException "No valid parameters sent to command\n$(getCommandHelpInfoMessage "${command_name}")" "${has_space_before}" "${has_space_after}"
+    return 0
 }
 
 # Display no sudo privileges message
-# $1 no-spaces-before
-# $2 no-spaces-after
+# $1 has_space_before
+# $2 has_space_after
 function noSudoPrivilegesException() {
-    declare no_spaces_before=$1 && declare no_spaces_after=$2
-    genericException "No $(color cyan)sudo$(color) privileges provided" ${no_spaces_before} ${no_spaces_after}
-    unset
+    declare has_space_before has_space_after
+    has_space_before=$1 && has_space_after=$2
+    genericException "No $(color cyan)sudo$(color) privileges provided" "${has_space_before}" "${has_space_after}"
+    return 0
 }
