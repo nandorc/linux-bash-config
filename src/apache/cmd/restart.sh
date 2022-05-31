ef6000591c504096932ac7f29ce22c4ccaf218d6
Daniel F. Rivera C <daniel.rivera@pragma.com.co>
2022-05-31 16:24:26 -0500

    Refactor apache command

--- src/apache/cmd/restart.sh
+++ src/apache/cmd/restart.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
 # Load dependencies
-source ~/.basher/lib/servicehandler.sh
-source ~/.basher/lib/flagger.sh
+source ~/.basher/lib/messagehandler.sh
+source ~/.basher/lib/flaghandler.sh
 
 # Check parameters
-declare service_name="apache"
-declare options=$*
-declare no_spaces=$(hasFlag --no-spaces ${options}) && options=$(pruneFlag --no-spaces ${options})
-declare is_compact=$(hasFlag --compact ${options}) && options=$(pruneFlag --compact ${options})
-[ -n "${options}" ] && noValidOptionsException ${service_name} ${no_spaces} ${no_spaces}
+declare service_name options spaces is_compact res_cod tmp
+service_name=apache
+options=$*
+spaces=$(hasFlag --no-spaces ${options}) && options=$(pruneFlag --no-spaces ${options})
+[ ${spaces} -eq 1 ] && spaces=0 || spaces=1
+is_compact=$(hasFlag --compact ${options}) && options=$(pruneFlag --compact ${options})
+[ -n "${options}" ] && noValidOptionsException "${service_name}" "${spaces}" "${spaces}"
 
 # Ask for sudo privileges if necessary to try to restart service
-wrap "$(color light-blue)INF~$(color) You may need to type your $(color cyan)sudo$(color) password to continue" ${no_spaces} 1 ${is_compact} 0
-sudo echo -e "EXE~ Trying to restart ${service_name} service...\c"
+mayNeedSudoMessage "${spaces}" 0 "${is_compact}"
+sudo echo -e "$(genericExecutionMessage "Trying to restart ${service_name} service..." 0 0)\c"
 if [ $? -eq 0 ]; then
-    declare res_cod=0
+    res_cod=0
     if [ $(basher "${service_name}":status --output bool) -eq 1 ]; then
         tmp=$(basher "${service_name}":stop 2>&1)
         res_cod=$?
-        [ ${res_cod} -ne 0 ] && processResponse ${res_cod} && genericException "${service_name} couldn't be stopped so restart process was aborted" 1 ${no_spaces}
+        [ ${res_cod} -ne 0 ] && responseString "${res_cod}" && genericException "${service_name} couldn't be stopped so restart process was aborted" 0 "${spaces}"
     fi
     tmp=$(basher "${service_name}":start 2>&1)
     res_cod=$?
-    [ ${res_cod} -ne 0 ] && echo -e "$(getResponseMessage ${res_cod})" && genericException "${service_name} couldn't be started so restart process was aborted" 1 ${no_spaces}
-    wrap "$(getResponseMessage ${res_cod})" 1 ${no_spaces} 0 0 && exit
+    [ ${res_cod} -ne 0 ] && responseString "${res_cod}" && genericException "${service_name} couldn't be started so restart process was aborted" 0 "${spaces}"
+    wrap "$(responseString "${res_cod}")" 0 "${spaces}" 0 0 && exit 0
 else
-    noSudoPrivilegesException 1 ${no_spaces}
+    noSudoPrivilegesException 0 "${spaces}"
 fi
--- src/apache/cmd/start.sh
+++ src/apache/cmd/start.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
 # Load dependencies
-source ~/.basher/lib/servicehandler.sh
-source ~/.basher/lib/flagger.sh
+source ~/.basher/lib/messagehandler.sh
+source ~/.basher/lib/flaghandler.sh
 
 # Check parameters
-declare service_name="apache"
-declare options=$*
-declare no_spaces=$(hasFlag --no-spaces ${options}) && options=$(pruneFlag --no-spaces ${options})
-declare is_compact=$(hasFlag --compact ${options}) && options=$(pruneFlag --compact ${options})
-[ -n "${options}" ] && noValidOptionsException ${service_name} ${no_spaces} ${no_spaces}
+declare service_name options spaces is_compact tmp res_cod
+service_name=apache
+options=$*
+spaces=$(hasFlag --no-spaces ${options}) && options=$(pruneFlag --no-spaces ${options})
+[ ${spaces} -eq 1 ] && spaces=0 || spaces=1
+is_compact=$(hasFlag --compact ${options}) && options=$(pruneFlag --compact ${options})
+[ -n "${options}" ] && noValidOptionsException "${service_name}" "${spaces}" "${spaces}"
 
 # Check and start service if necessary
-[ $(basher "${service_name}":status --output bool) -eq 1 ] && wrap "$(color light-blue)INF~$(color) ${service_name} service is currently running" ${no_spaces} ${no_spaces} 0 0 && exit
-wrap "$(color light-blue)INF~$(color) You may need to type your $(color cyan)sudo$(color) password to continue" ${no_spaces} 1 ${is_compact} 0
-sudo echo -e "EXE~ Trying to start ${service_name} service...\c"
+[ $(basher "${service_name}":status --output bool) -eq 1 ] && genericInfoMessage "${service_name} service is currently running" "${spaces}" "${spaces}" && exit 0
+mayNeedSudoMessage "${spaces}" 0 "${is_compact}"
+sudo echo -e "$(genericExecutionMessage "Trying to start ${service_name} service..." 0 0)\c"
 if [ $? -eq 0 ]; then
     tmp=$(sudo service apache2 start 2>&1)
     res_cod=$?
-    wrap "$(getResponseMessage ${res_cod})" 1 ${no_spaces} 0 0 && exit ${res_cod}
+    wrap "$(responseString ${res_cod})" 0 "${spaces}" 0 0 && exit ${res_cod}
 else
-    noSudoPrivilegesException 1 ${no_spaces}
+    noSudoPrivilegesException 0 ${spaces}
 fi
