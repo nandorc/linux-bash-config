6d2173e0a4466e2fb6df0ef111d671d5c804dd4f
Daniel F. Rivera C <daniel.rivera@pragma.com.co>
2022-06-01 23:51:19 -0500

    Refactor lamp command actions

--- src/lamp/cmd/restart.sh
+++ src/lamp/cmd/restart.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-# Dependencies
-source ~/.basher/lib/wrapper.sh
+# Load dependencies
+source ~/.basher/lib/messagehandler.sh
+source ~/.basher/lib/flaghandler.sh
 
-options=$(getRebuildedOptions $*)
-printColoredMessage "<LAMPServicesRestart>" --wrap-position begin $options
-# Restart related services
-basher apache:restart --no-color --spacing none
-basher mysql:restart --no-color --spacing none
-# End message
-printColoredMessage "</LAMPServicesRestart>" --wrap-position end $options
-unset options
+# Check parameters
+declare service_name options spaces is_compact
+service_name=lamp
+options=$*
+spaces=$(hasFlag --no-spaces ${options}) && options=$(pruneFlag --no-spaces ${options})
+[ ${spaces} -eq 1 ] && spaces=0 || spaces=1
+is_compact=$(hasFlag --compact ${options}) && options=$(pruneFlag --compact ${options})
+[ -n "${options}" ] && noValidOptionsException "${service_name}" "${spaces}" "${spaces}"
+
+# Start services
+commandBlockHeading "LAMP Services Restart" "${spaces}" 0
+mayNeedSudoMessage 0 0 "${is_compact}"
+basher apache:restart --no-spaces --compact
+basher mysql:restart --no-spaces --compact
+[ ${spaces} -eq 1 ] && echo ""
