f76be07d85170b1f2db19e262e0d596227e15edf
Daniel F. Rivera C <daniel.rivera@pragma.com.co>
2022-06-03 21:33:33 -0500

    Refactor actions for magento command

--- src/magento/cmd/start.sh
+++ src/magento/cmd/start.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-# Dependencies
-source ~/.basher/lib/wrapper.sh
+# Load dependencies
+source ~/.basher/lib/messagehandler.sh
+source ~/.basher/lib/flaghandler.sh
 
-options=$(getRebuildedOptions $*)
-printColoredMessage "<MagentoServicesStart>" --wrap-position begin $options
-# Begin service starting process
-basher apache:start --no-color --spacing none
-basher mysql:start --no-color --spacing none
-basher elasticsearch:start --no-color --spacing none
-# End service starting process
-printColoredMessage "</MagentoServicesStart>" --wrap-position end $options
-unset options
+# Check parameters
+declare service_name options spaces is_compact
+service_name=magento
+options=$*
+spaces=$(hasFlag --no-spaces ${options}) && options=$(pruneFlag --no-spaces ${options})
+[ ${spaces} -eq 1 ] && spaces=0 || spaces=1
+is_compact=$(hasFlag --compact ${options}) && options=$(pruneFlag --compact ${options})
+[ -n "${options}" ] && noValidOptionsException "${service_name}" "${spaces}" "${spaces}"
+
+# Start services
+commandBlockHeading "Magento Services Start" "${spaces}" 0
+mayNeedSudoMessage 0 0 "${is_compact}"
+basher apache:start --no-spaces --compact
+basher mysql:start --no-spaces --compact
+basher docker:start --no-spaces --compact
+basher elasticsearch:start --no-spaces
+[ ${spaces} -eq 1 ] && echo ""
